9cbb15b5913750f147e474ae3e39044cb4fff431
adiMallya <adimallya99@gmail.com>
2022-04-26 17:58:10 +0530

    add : script for alert

--- components/components.js
+++ components/components.js
@@ -1,3 +1,16 @@
+// Alerts
+const btnShowAlert = document.querySelector('.show-alert-example-btn');
+const alert = document.getElementById('alert-example-warning');
+const btnCloseAlert = document.getElementById('alert-example-close-btn');
+
+btnShowAlert.addEventListener('click', () => {
+  alert.style.display = alert.style.display === 'none' ? 'block':'none';
+});
+
+btnCloseAlert.addEventListener('click', () => {
+  alert.style.display = 'none';
+});
+
 // Toast
 const btnShowToast = document.querySelector('.show-toast-example-btn');
 const toast = document.getElementById('toast-example-success');
